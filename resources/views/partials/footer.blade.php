902014dd5c213503b6a999aa182a70e8fc5b5ddb
Armin <kzardkoohi@gmail.com>
2017-07-21 15:59:31 -0500

    changed design for footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,6 @@
 <div class="container">
-  <div class="row text-center">
+  <div class="row text-center" style="padding-bottom: 20px;">
+    <hr>
     &copy; Copyright Acme Company 2017
   </div>
 </div>
