b15bded74921b33b71f98df17b5b61434229d7c9
Armin <kzardkoohi@gmail.com>
2017-07-21 04:43:10 -0500

    added new content to template files

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,12 +2,21 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>Acme</title>
   <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  @yield('content')
-  @include('partials.sidebar')
+  @include('partials.nav')
+  <div class="container" style="margin-top: 50px;">
+    <div class="row">
+      <div class="col-lg-9 col-md-8">
+        @yield('content')
+      </div>
+      <div class="col-lg-3 col-md-4">
+        @include('partials.sidebar')
+      </div>
+  </div>
+
 
 </body>
 </html>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,5 +1,6 @@
 @section('sidebar')
-<div class="sidebar">
-  <h3>This is my sidebar</h3>
-  <p>Here is some more content</p>
+<div class="well" style="margin-top: 30px;">
+  <h3>Sidebar</h3>
+  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+  <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 </div>
