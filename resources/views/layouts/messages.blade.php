9fe3e3c15815dfa1690f362ac7d6a27caab0de0c
Armin <kzardkoohi@gmail.com>
2017-07-24 01:13:09 -0500

    fixed closing list tag

--- resources/views/layouts/messages.blade.php
+++ resources/views/layouts/messages.blade.php
@@ -7,6 +7,7 @@
       <li class="list-group-item">Name: {{$message->name}}</li>
       <li class="list-group-item">Email: {{$message->email}}</li>
       <li class="list-group-item">Message: {{$message->message}}</li>
+    </ul>
   @endforeach
 @endif
 @endsection
